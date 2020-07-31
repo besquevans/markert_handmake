5bdbefcadaedbe87bd0408c90d7af73cd97869b6
Gavin Liu <du20783@gmail.com>
2020-07-31 18:08:56 +0800

    add controller products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+  before_action :user_auth
+
+  def index
+    @products = Product.all
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save 
+      redirect_to produts_path, notice: "Create success!"
+    else
+      render :new 
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+  
+  end
+  
+  def show
+  
+  end
+  
+  def destroy
+  
+  end
+
+
+  private 
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+  def user_auth 
+    if !current_user
+      redirect_to new_user_path
+    end
+  end
+
+end
