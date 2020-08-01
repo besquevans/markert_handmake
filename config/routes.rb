c38e0636bdd1c75dd0b048fe64a1d2c6c81a5957
Gavin Liu <du20783@gmail.com>
2020-08-01 11:16:14 +0800

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :products
+  resources :products, as: 'products'
 end
